f5396b76d7fd099df6c0c371247ac434fbd36858
Armando Miranda <adservador@gmail.com>
2022-03-31 23:50:58 +0200

    GENERAL: -Signup Token Done -Password encrypted

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,9 +1,15 @@
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 const UserModel = require("../models/user.model")
 
 async function signup(req, res) {
   try {
+    const hash = bcrypt.hashSync(req.body.password, 10)
+    req.body.password = hash
     const user = await UserModel.create(req.body)
-    res.status(200).send('You have signed up successfully!')
+    const payload = {email: user.email}
+    const token = jwt.sign(payload, process.env.SECRET, {expiresIn: '1h'})
+    res.status(200).json({email: user.email, token: token})
   } catch (error) {
     res.status(500).send(`Sign up error ${error}`)
   }
