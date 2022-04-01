0a836efea912fa0b391cb386e6ae8fb41fa56a2d
Armando Miranda <adservador@gmail.com>
2022-04-02 00:19:27 +0200

    GENERAL -Auth Router Completed and Tested

--- api/routes/auth.router.js
+++ api/routes/auth.router.js
@@ -1,7 +1,8 @@
 const router = require('express').Router()
 
-const {signup} = require ('../controllers/auth.controller')
+const {signup, login} = require ('../controllers/auth.controller')
 
 router.post('/signup', signup)
+router.post('/login', login)
 
 module.exports = router
