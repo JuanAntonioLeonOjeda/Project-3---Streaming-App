458c1f2ce980ed237f7697115f5b6cdbe3c4f2e1
David Pallarés <serallapdivad@gmail.com>
2022-04-04 00:20:25 +0100

    ACCESSFORM - @nuxtjs/auth added - BaseURL added

--- front/nuxt.config.js
+++ front/nuxt.config.js
@@ -47,7 +47,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: '/'
+    baseURL: 'http://localhost:5500/api'
   },
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
