42766dc2862bf399f8074085459d01a2dd5740f7
David Pallarés <serallapdivad@gmail.com>
2022-04-03 01:35:50 +0100

    GENRES - Genre Model Created - Genre Router Created - Genre Controller Created ( Get All Genres, Get One Genre, ByAdmin: ( Add Genre, Update Genre, Remove Genre ))

--- api/routes/index.js
+++ api/routes/index.js
@@ -2,9 +2,11 @@ const router = require('express').Router()
 
 const authRouter = require('./auth.router')
 const userRouter = require('./user.router')
+const genreRouter = require('./genre.router')
 
 router
   .use('/auth', authRouter)
   .use('/users', userRouter)
+  .use('/genres', genreRouter)
 
 module.exports = router;
