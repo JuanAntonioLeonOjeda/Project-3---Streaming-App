ec0ac94c3b67dea6f22538b4a3a7295a8913eb16
David Pallarés <serallapdivad@gmail.com>
2022-04-04 19:40:42 +0100

    NAVIGATOR DRAWER -Navigator drawer created

---
+++ front/components/NavigationDrawer.vue
@@ -0,0 +1,75 @@
+<template>
+  <v-navigation-drawer
+    app
+    expand-on-hover
+  >
+    <v-list>
+      <v-list-item class="px-2">
+        <v-list-item-avatar>
+          <v-img src="https://images.pexels.com/photos/4566232/pexels-photo-4566232.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+        </v-list-item-avatar>
+      </v-list-item>
+
+      <v-list-item link>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6">
+            ElUserNameTuyo
+          </v-list-item-title>
+          <v-list-item link>
+            <v-icon>mdi-account-circle</v-icon>
+            <v-list-item-subtitle>My Profile</v-list-item-subtitle>
+            <v-list-item link />
+          </v-list-item>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+
+    <v-divider />
+
+    <v-list
+      nav
+      dense
+    >
+      <v-list-item link>
+        <v-list-item-icon>
+          <v-icon>mdi-record</v-icon>
+        </v-list-item-icon>
+        <v-list-item-title>My Streams</v-list-item-title>
+      </v-list-item>
+      <v-list-item link>
+        <v-list-item-icon>
+          <v-icon>mdi-account-group</v-icon>
+        </v-list-item-icon>
+        <v-list-item-title>Friends</v-list-item-title>
+      </v-list-item>
+      <v-list-item link>
+        <v-list-item-icon>
+          <v-icon>mdi-message-reply</v-icon>
+        </v-list-item-icon>
+        <v-list-item-title>Whispers</v-list-item-title>
+      </v-list-item>
+      <v-list-item link>
+        <v-list-item-icon>
+          <v-icon>mdi-police-badge</v-icon>
+        </v-list-item-icon>
+        <v-list-item-title>Badges</v-list-item-title>
+      </v-list-item>
+      <v-list-item link>
+        <v-list-item-icon>
+          <v-icon>mdi-shuffle-variant</v-icon>
+        </v-list-item-icon>
+        <v-list-item-title>Shuffle</v-list-item-title>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  name: 'NavigationDrawer'
+
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
--- front/layouts/default.vue
+++ front/layouts/default.vue
@@ -5,12 +5,6 @@
         <Nuxt />
       </v-container>
     </v-main>
-    <v-footer
-      :absolute="!fixed"
-      app
-    >
-      <span>&copy; {{ new Date().getFullYear() }}</span>
-    </v-footer>
   </v-app>
 </template>
 
