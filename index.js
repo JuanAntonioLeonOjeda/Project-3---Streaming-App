e5992b22c19327e6da267f88fe964ff49eb621bd
root <root@LAPTOP-LMJQULD0.localdomain>
2022-04-06 20:54:37 +0100

    back index.js - cors url modified to netlify

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const app = express()
 const httpServer = createServer(app)
 const io = new Server (httpServer, {
   cors: {
-    origin:'http://localhost:3000'
+    origin:'https://peek-beats.netlify.app/'
   }
 })
 
