8d7c1f8704dd755b52f757913931a70229d5f7be
David Pallarés <serallapdivad@gmail.com>
2022-04-14 12:30:19 +0100

    Updated DB URL

--- front/nuxt.config.js
+++ front/nuxt.config.js
@@ -68,7 +68,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'http://localhost:5000/api'
+    baseURL: 'https://peek-beats.herokuapp.com/api'
   },
 
   auth: {
